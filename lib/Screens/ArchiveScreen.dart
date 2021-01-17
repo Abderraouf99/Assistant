04078335eb2736ab7fc1cbf7774f7e5cfdd5940f
Abderraouf99 <55321747+Abderraouf99@users.noreply.github.com>
2021-01-17 01:59:58 -0500

    added colring to the archive app bar

--- lib/Screens/ArchiveScreen.dart
+++ lib/Screens/ArchiveScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/Widgets/EventArchiveList.dart';
 import 'package:to_do_app/Widgets/NoteArchieveList.dart';
-import 'package:to_do_app/Widgets/NotesList.dart';
 import 'package:to_do_app/Widgets/PageHeader.dart';
 import 'package:to_do_app/Widgets/SideNavigationDrawer.dart';
 import 'package:to_do_app/Widgets/TaskArchieveList.dart';
@@ -39,34 +38,27 @@ class _ArchiveScreenState extends State<ArchiveScreen> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  FlatButton(
-                    autofocus: true,
-                    child: Icon(
-                      Icons.list,
-                      color: Color(0xffEEEEEE),
-                    ),
+                  TopBarSelectors(
+                    iconData: Icons.list,
+                    colorCondition: (page == 0),
                     onPressed: () {
                       setState(() {
                         page = 0;
                       });
                     },
                   ),
-                  FlatButton(
-                    child: Icon(
-                      Icons.event,
-                      color: Color(0xffEEEEEE),
-                    ),
+                  TopBarSelectors(
+                    iconData: Icons.event,
+                    colorCondition: (page == 1),
                     onPressed: () {
                       setState(() {
                         page = 1;
                       });
                     },
                   ),
-                  FlatButton(
-                    child: Icon(
-                      Icons.note,
-                      color: Color(0xffEEEEEE),
-                    ),
+                  TopBarSelectors(
+                    iconData: Icons.note,
+                    colorCondition: (page == 2),
                     onPressed: () {
                       setState(() {
                         page = 2;
@@ -86,6 +78,35 @@ class _ArchiveScreenState extends State<ArchiveScreen> {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class TopBarSelectors extends StatelessWidget {
+  final Function onPressed;
+  final bool colorCondition;
+  final IconData iconData;
+  TopBarSelectors(
+      {@required this.onPressed,
+      @required this.colorCondition,
+      @required this.iconData});
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      shape: CircleBorder(),
+      onPressed: onPressed,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        decoration: colorCondition
+            ? BoxDecoration(
+                color: Color(0xffEEEEEE),
+                borderRadius: BorderRadius.circular(20))
+            : null,
+        child: Icon(
+          iconData,
+          color: colorCondition ? Color(0xff222831) : Color(0xffEEEEEE),
         ),
       ),
     );
